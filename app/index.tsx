04898094a1ef93855a10bc7d244b820848ad78bf
WU Yue <yuewuxd@gmail.com>
2024-12-24 23:51:33 +0800

    improvement

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,38 @@
-import { Text, View, SafeAreaView, StyleSheet } from "react-native";
 import * as ScreenOrientation from "expo-screen-orientation";
+import { Text, View, SafeAreaView, StyleSheet, Animated } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useEffect, useRef } from "react";
 
 export default function Index() {
+  // Lock screen orientation
   ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
+
+  // FadeInOpacity starts at 0 and smoothly increases to 1 over the course of 1 second
+  const fadeInOpacity = useRef(new Animated.Value(0)).current;
+  useEffect(() => {
+    Animated.sequence([
+      Animated.timing(fadeInOpacity, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.welcomeText}>Hello</Text>
-      <Text style={styles.subtitleText}>my name is</Text>
-      <View style={styles.textboxContainer}>
-        <Text style={styles.textbox}>✨ WU Yue ✨</Text>
-      </View>
+      {/* Gradient background */}
+      <LinearGradient colors={["#FF3E3E", "#FF7676", "#FFB3B3"]} style={styles.background} />
+
+      {/* Content container that fades in on app load */}
+      <Animated.View style={[styles.contentContainer, { opacity: fadeInOpacity }]}>
+        {/* Name badge content */}
+        <Text style={[styles.welcomeText, styles.shadow]}>Hello</Text>
+        <Text style={[styles.subtitleText, styles.shadow]}>my name is</Text>
+        <View style={[styles.textboxContainer, styles.shadow]}>
+          <Text style={styles.textbox}>✨ WU Yue ✨</Text>
+        </View>
+      </Animated.View>
     </SafeAreaView>
   );
 }
@@ -18,9 +40,19 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: "red", // fallback color
+  },
+  contentContainer: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "red",
   },
   welcomeText: {
     fontSize: 90,
@@ -48,5 +80,12 @@ const styles = StyleSheet.create({
     fontSize: 60,
     textAlign: "center",
     fontWeight: "bold",
+  },
+  shadow: {
+    elevation: 5,
+    shadowColor: "black",
+    shadowOffset: { width: 5, height: 5 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
   },
 });
